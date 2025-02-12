bf1cef18b5de78c2e23efc78bfa87e86e624854b
john santos <jhow.mito11@gmail.com>
2025-02-12 11:43:28 -0300

    criptografando senha

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 
 import { CreateUserController } from "./controllers/user/CreatedUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 
 //fronte-end chama a rota primeiro, depois chama o controler
 
 const router =Router();
 
 //--ROTAS USER
-router.post('/users', new CreateUserController().handle)
+router.post('/users', new CreateUserController().handle);
+
+router.post ('/login',new AuthUserController().handle);
 
 
 export  {router}
--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,14 +1,37 @@
 import prismaClient from "../../prisma";
+import { compare } from "bcryptjs";
+
 
 interface AuthRequest{
     email: string;
-    passWord: string
+    password: string
 }
 
 class AuthUserService{
 
-    async execute(){
+    async execute({email,password}: AuthRequest){   
+        //verificar se email existe
+        const user = await prismaClient.user.findFirst({
+            where:{
+                email:email
+            }
+        })
+
+        if(!user){
+            throw new Error("Usuario ou senha incorreta")
+        }
+
+        // verificar se a senha esta correta..
+        const passwordMatch = await compare(password,user.password)
         
+        if(!passwordMatch){
+        throw new Error("Usuario ou senha incorreta")
+        }
+
+
+        return{ok:true}
     }
     
 }
+
+export {AuthUserService};
