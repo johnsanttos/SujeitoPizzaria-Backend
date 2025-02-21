91668f152af360282e8a72b7fa7ada83edf9c5ab
John Santos <jhow.mito11@gmail.com>
2025-02-21 15:45:59 -0300

    Rota de produtos

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 
 //fronte-end chama a rota primeiro, depois chama o controler
 
@@ -24,5 +25,9 @@ router.get('/me', isAuthenticated ,new DetailUserController().handle);
 router.post('/category', isAuthenticated,new CreateCategoryController().handle)
 
 router.get('/category', isAuthenticated,new ListCategoryController().handle)
+
+//--ROTAS PRODUCT--
+
+router.post('/product', isAuthenticated,new CreateProductController().handle)
 
 export  {router}
