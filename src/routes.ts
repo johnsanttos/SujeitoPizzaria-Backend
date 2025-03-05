df8b7d3bce153d32165398178f693d52cbe52d34
John Santos <jhow.mito11@gmail.com>
2025-03-05 15:35:26 -0300

    Enviando um pedido

--- src/routes.ts
+++ src/routes.ts
@@ -13,6 +13,7 @@ import  uploadConfig from './config/multer'
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController";
 
 
 //fronte-end chama a rota primeiro, depois chama o controler
@@ -52,5 +53,7 @@ router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
 
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 export  {router}
 
