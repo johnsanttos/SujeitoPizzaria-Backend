e7d38ba3b7b07b3f087575433512494295310299
John Santos <jhow.mito11@gmail.com>
2025-03-07 14:53:20 -0300

    funcuinalidades completa

--- src/routes.ts
+++ src/routes.ts
@@ -16,6 +16,7 @@ import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
 import { ListOrderController } from "./controllers/order/ListOrderController";
 import { DetailOrderController } from "./controllers/order/DetailOrderController";
+import { FinishOrderController } from "./controllers/order/FinishOrderController";
 
 
 //fronte-end chama a rota primeiro, depois chama o controler
@@ -61,5 +62,7 @@ router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 router.get('/orders', isAuthenticated, new ListOrderController().handle)
 
 router.get('/orders/detail', isAuthenticated, new DetailOrderController().handle)
+
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
 export  {router}
 
